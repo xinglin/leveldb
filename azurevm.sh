e55e2d2348ce760f3895be5b644f6527f772c7c9
Xing Lin <linxingnku@gmail.com>
2021-08-15 22:01:22 -0700

    azurevm.sh: minor

--- azurevm.sh
+++ azurevm.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install git cmake g++
+sudo apt install git cmake g++ fish
+# git clone --recurse-submodules https://github.com/xinglin/leveldb.git
+cd leveldb; mkdir build; git checkout vectorkv;
+pwd
 
